24ffbd3f7ef0f050bc28d90236b3e0452cff147c
hangone <56105779+hangone@users.noreply.github.com>
2025-08-21 01:02:25 +0800

    fix: [vite:resolve] Failed to resolve entry for package "vite-ssr".

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import routes from "./router";
 import App from "./App";
-import viteSSR from "vite-ssr";
+// import viteSSR from "vite-ssr";
 import { showHairMessage } from "@/utils/hair";
 import "./index.css";
 import "reset.css/reset.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-export default viteSSR(App, { routes }, (context) => {
-  /* Vite SSR main hook for custom logic */
-  /* const { app, router, initialState, ... } = context */
-});
+// export default viteSSR(App, { routes }, (context) => {
+//   /* Vite SSR main hook for custom logic */
+//   /* const { app, router, initialState, ... } = context */
+// });
 
 if (import.meta.env.PROD) {
   showHairMessage();
